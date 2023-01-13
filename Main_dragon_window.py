93272b27523581bb5164f2b12e42ddf98136eee1
VeraPavlovskaya <Vera.Pavlovskaya2008@yandex.ru>
2023-01-14 03:36:34 +0800

    Selecting levels button

--- Main_dragon_window.py
+++ Main_dragon_window.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         pygame.init()
         # размеры окна:
         size = width, height  # = 800, 600
-        color = [255, 30, 200]
+        # color = [255, 30, 200]
         pygame.display.set_caption('Main Window')
         # screen — холст, на котором нужно рисовать:
         screen = pygame.display.set_mode(size)
@@ -38,26 +38,31 @@ if __name__ == '__main__':
         # pygame.draw.rect(screen, color, [1, 1, width - 2, height - 2])
         fon = pygame.transform.scale(load_image('bg2.jpg'), (650, 480))
         store = pygame.transform.scale(load_image('store.png'), (80, 80))
-        profie = pygame.transform.scale(load_image('profile2.png'), (100, 90))
+        profie = pygame.transform.scale(load_image('profile2.png'), (80, 80))
         coin = pygame.transform.scale(load_image('coins.png'), (50, 50))
         dragon_collaction = pygame.transform.scale(load_image('dragon_collaction.png'), (80, 80))
+        choose_level = pygame.transform.scale(load_image('select_level.crdownload'), (180, 60))
+        instructions = pygame.transform.scale(load_image('instructions.jfif'), (50, 50))
+
 
         font1 = pygame.font.SysFont('freesanbold.ttf', 30)
-        text1 = font1.render('My Profile', True, (0, 255, 0))
+        text1 = font1.render('My Profile', True, (0, 0, 0))
         text2 = font1.render('Dragon rings:', True, (0, 0, 0))
 
         textRect1 = text1.get_rect()
         textRect2 = text2.get_rect()
-        textRect1.center = (80, 140)
+        textRect1.center = (70, 160)
         textRect2.center = (250, 50)
 
         run = True
         while run:
             screen.blit(fon, (0, 0))
-            screen.blit(store, (40, 250))
-            screen.blit(profie, (30, 30))
+            screen.blit(store, (30, 250))
+            screen.blit(profie, (30, 70))
             screen.blit(coin, (330, 20))
-            screen.blit(dragon_collaction, (40, 200))
+            screen.blit(dragon_collaction, (30, 200))
+            screen.blit(choose_level, (250, 400))
+            screen.blit(instructions, (570, 30))
             screen.blit(text1, textRect1)
             screen.blit(text2, textRect2)
             pygame.display.flip()
