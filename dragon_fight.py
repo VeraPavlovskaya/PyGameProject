ff1dc2559d0743e81fce095a6cc626d203989662
stepan <stepan@k0mp.ru>
2023-01-15 17:05:17 +0300

    основы геймплея

--- dragon_fight.py
+++ dragon_fight.py
@@ -3,9 +3,14 @@ import sys
 import pygame as pg
 
 FPS = 60
-SIZE = WIDTH, HEIGHT = 600, 480
-STEP = 1   # Максимально плавное движение драконов
-SPEED = 100  # Пикселей в секунду
+SPEED = 1   # Максимально плавное движение драконов
+pg.init()
+pg.key.set_repeat(1, 1)
+sc = pg.display.set_mode((800, 450))
+clock = pg.time.Clock()
+all_sprites = pg.sprite.Group()
+players_group = pg.sprite.Group()
+tiles_group = pg.sprite.Group()
 
 
 def terminate():
@@ -33,55 +38,110 @@ TILE_IMAGES = {
     'wall': load_image('box.png'),
     'empty': load_image('grass.png')
 }
-DRAGON_IMAGE1 = load_image('none')   # Жду моделек
-DRAGON_IMAGE2 = load_image('none')
+TILE_WIDTH = TILE_HEIGHT = 50
+DRAGON_WIDTH = DRAGON_HEIGHT = 75
+
+
+def load_level(filename):
+    filename = "maps/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return [line.ljust(max_width, '.') for line in level_map]
+
+
+class Tile(pg.sprite.Sprite):
+    def __init__(self, tile_type, x, y):
+        super().__init__(all_sprites, tiles_group)
+        self.image = pg.transform.scale(TILE_IMAGES[tile_type], (TILE_WIDTH, TILE_HEIGHT))
+        self.rect = self.image.get_rect().move(TILE_WIDTH * x, TILE_HEIGHT * y)
+
+
+def generate_level(level):
+    player1, player2, x, y = None, None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                if player1:
+                    player2 = Dragon(2, x, y, 'right')
+                else:
+                    player1 = Dragon(1, x, y, 'left')
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+    return player1, player2
 
 
 class Dragon(pg.sprite.Sprite):
-    def __init__(self, *args):
-        groups = args[:-1]
-        dragon_num = args[-1]
-        super().__init__(*groups)
-        if dragon_num == 1:
-            self.image = DRAGON_IMAGE1
+    def __init__(self, dragon_id, x, y, dir_):
+        super().__init__(all_sprites, players_group)
+        if dragon_id == 1:
+            pic = air_dragon
+        elif dragon_id == 2:
+            pic = earth_dragon
+        elif dragon_id == 3:
+            pic = fire_dragon
+        elif dragon_id == 4:
+            pic = water_dragon
         else:
-            self.image = DRAGON_IMAGE2
-        self.rect = self.image.get_rect()
-        self.mask = pg.mask.from_surface(self.image)
+            raise SystemExit('Ошибка при вызове класса Dragon')
+        self.image = pg.transform.flip(pg.transform.scale(pic, (DRAGON_WIDTH, DRAGON_HEIGHT)), True, False)
+        self.rect = self.image.get_rect().move(TILE_WIDTH * x - 25, TILE_HEIGHT * y - 25)
+        if dir_ == 'left':
+            self.dir = 1
+        elif dir_ == 'right':
+            self.dir = -1
+        else:
+            raise SystemExit('Ошибка при вызове класса Dragon')
 
-    def move(self, direction):
-        dx = direction[0] * SPEED
-        dy = direction[1] * SPEED
-        self.rect = self.rect.move(dx, dy)
+    def update(self, dir_x, dir_y):
+        self.rect.x += SPEED * dir_x
+        if (self.dir == 1 and dir_x == -1) or (self.dir == -1 and dir_x == 1):
+            self.image = pg.transform.flip(self.image, True, False)
+            self.dir *= -1
+        self.rect.y += SPEED * dir_y
+
+
+air_dragon = load_image('fire_dragon.png')
+earth_dragon = load_image('earth_dragon.png')
+fire_dragon = load_image('fire_dragon.png')
+water_dragon = load_image('water_dragon.png')
 
 
 def main():
-    pg.init()
-    pg.key.set_repeat(200, 70)
-    sc = pg.display.set_mode(SIZE)
-    clock = pg.time.Clock()
-    all_sprites = pg.sprite.Group()
-    players_group = pg.sprite.Group()
-    tiles_group = pg.sprite.Group()
-    player1 = Dragon(all_sprites, players_group, 1)
-    player2 = Dragon(all_sprites, players_group, 2)
-    running = True
-    while running:
+    player1, player2 = generate_level(load_level('map1.txt'))
+    while True:
+        dx1 = dy1 = dx2 = dy2 = 0
         for event in pg.event.get():
-            if event.type == pg.K_w:
-                player1.move((0, -1))
-            if event.type == pg.K_a:
-                player1.move((-1, 0))
-            if event.type == pg.K_s:
-                player1.move((0, 1))
-            if event.type == pg.K_d:
-                player1.move((1, 0))
-            if event.type == pg.K_UP:
-                player2.move((0, 1))
-            if event.type == pg.K_LEFT:
-                player2.move((-1, 0))
-            if event.type == pg.K_DOWN:
-                player2.move((0, 1))
-            if event.type == pg.K_RIGHT:
-                player2.move((1, 0))
+            if event.type == pg.QUIT:
+                terminate()
+            if event.type == pg.KEYDOWN:
+                pressed_keys = pg.key.get_pressed()
+                if pressed_keys[pg.K_w]:
+                    dy1 -= 1
+                if pressed_keys[pg.K_a]:
+                    dx1 -= 1
+                if pressed_keys[pg.K_s]:
+                    dy1 += 1
+                if pressed_keys[pg.K_d]:
+                    dx1 += 1
+                if pressed_keys[pg.K_UP]:
+                    dy2 -= 1
+                if pressed_keys[pg.K_LEFT]:
+                    dx2 -= 1
+                if pressed_keys[pg.K_DOWN]:
+                    dy2 += 1
+                if pressed_keys[pg.K_RIGHT]:
+                    dx2 += 1
+        player1.update(dx1, dy1)
+        player2.update(dx2, dy2)
+        tiles_group.draw(sc)
+        players_group.draw(sc)
+        pg.display.flip()
+        clock.tick(FPS)
 
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import pygame
+import pygame as pg
+import dragon_fight
+
 
 if __name__ == '__main__':
-    # инициализация Pygame:
-    width = 700
-    height = 750
-    print('Неправильный формат ввода')
-    exit()
-    pygame.init()
-    # размеры окна:
-    white = [255, 255, 255]
-    pygame.display.set_caption('Шахматы')
-    # screen — холст, на котором нужно рисовать:
-    screen = pygame.display.set_mode((width, height))
+    dragon_fight.main()
+    pass
